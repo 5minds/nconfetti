fd87ecc1fbbee16106033ca9d1c4016f0cce2216
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2016-10-13 22:55:02 +0200

    Add sync version of json ref resolver

---
+++ tests/ref_resolver/test_simple.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const should = require('should');
+
+const refResolver = require('./../../lib/ref_resolver');
+
+const schema = require('./schema.json');
+
+describe('RefResolver', () => {
+
+  it('should resolve refs in schema synchronous', () => {
+    const resolvedSchema = refResolver(schema);
+
+    should(resolvedSchema.string).eql('string');
+  });
+
+});
