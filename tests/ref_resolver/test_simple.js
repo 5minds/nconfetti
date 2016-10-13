75f5e52178788359d1d5c0307fb5d13b48b068b8
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2016-10-13 23:00:19 +0200

    Add test for already resolved schema

--- tests/ref_resolver/test_simple.js
+++ tests/ref_resolver/test_simple.js
@@ -5,14 +5,22 @@ const should = require('should');
 const refResolver = require('./../../lib/ref_resolver');
 
 const schema = require('./schema.json');
+const resolvedSchema = require('./resolvedSchema.json');
 
 describe('RefResolver', () => {
 
   it('should resolve refs in schema synchronous', () => {
-    const resolvedSchema = refResolver(schema);
+    const resolved = refResolver(schema);
 
-    should(resolvedSchema.string)
+    should(resolved.string)
       .eql('string');
+  });
+
+  it('should work with already resolved schema', () => {
+    const resolved = refResolver(resolvedSchema);
+
+    should(resolved)
+      .deepEqual(resolvedSchema);
   });
 
 });
