87b831bac8fa37bad804492747838d1f8e354ecd
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2016-10-14 21:52:27 +0200

    Move sample to samples folder

--- samples/simple.js
+++ samples/simple.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const nconf = require('nconf');
 
-require('./lib/nconf_dir');
+require('./../lib/nconf_dir');
 
 nconf.argv()
   .env('__');
 
-nconf.use('Nconfdir', {dir: path.join(__dirname, 'tests', 'configs', 'env'), env: 'development'});
+nconf.use('Nconfdir', {dir: path.join(__dirname, '..', 'tests', 'configs', 'env'), env: 'development'});
 
 console.log(nconf.get('development_config:development_config'));
 
